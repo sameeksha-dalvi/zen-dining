cb99c2dccc55baa72364c7950c750d299ca24b9d
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2025-12-23 12:41:47 +0530

    feat: load modules on nav click

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 import "./styles.css";
 import loadHome from "./home.js"
-
 loadHome();
-console.log("Hi Sameeksha!!");
+
+
+
+const navBtnClick = document.querySelector('.nav-bar');
+
+navBtnClick.addEventListener('click',function(event){
+
+    if(event.target.closest('#home')){
+        loadHome();
+    }
+
+    if(event.target.closest('#menu')){
+        alert("menu clicked");
+    }
+
+    if(event.target.closest('#contact')){
+        alert("contact clicked");
+    }
+});
