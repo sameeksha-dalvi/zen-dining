324f38367c3ae45b8cb0cce15f684dc900165645
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2025-12-26 16:16:48 +0530

    chore: update webpack scripts and modes

--- webpack.common.js
+++ webpack.common.js
@@ -1,23 +1,19 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: "development",
+
     entry: "./src/index.js",
-    output: {
+    plugins: [
+        new HtmlWebpackPlugin({
+             template: "./src/template.html",
+        }),
+    ],
+     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
         clean: true,
     },
-    devtool: "eval-source-map",
-    devServer: {
-        watchFiles: ["./src/template.html"],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/template.html",
-        }),
-    ],
     module: {
         rules: [
             {
@@ -38,4 +34,5 @@ module.exports = {
             },
         ],
     },
+
 };
